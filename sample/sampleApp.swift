8de78d4c3e64c3c2727f390294f170cb987c275b
gwangjae-lee <gwangjae.lee@linecorp.com>
2021-11-28 21:42:53 +0900

    Initialize project

---
+++ sample/sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  sampleApp.swift
+//  sample
+//
+//  Created by user on 2021/11/28.
+//
+
+import SwiftUI
+
+@main
+struct sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
